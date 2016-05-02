3d01dd854899b9d8a0d5729d94e8d6e603fd3abd
R.A. Lucas <richard@richardalucas.com>
2016-05-01 18:39:18 -0600

    updated to handle no client on rest call

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function TwitterService(config) {
     this.client = new Twitter(this.config);
   } else {
     try {
-      getAppOnlyClient(this.config); 
+      this.getAppOnlyClient(this.config); 
     } catch (ex) {
       var error = new Error('Missing configuration necessary to create client');
       console.error(error, error.message, error.stack);
@@ -30,9 +30,26 @@ function TwitterService(config) {
   }
 }
 
+/**
+ * Calls the twitter rest client
+ * @param {String} method
+ * @param {String} endpoint
+ * @param {Object} params
+ **/
 TwitterService.prototype.callRest = function(method, endpoint, params, cb) {
+  var _this = this;
+
   params = params || {};
-  this.logger(method, endpoint, params);
+
+  if (!this.client) {
+    // Create one and then call this again
+    // TODO: There needs to be a better way here
+    return this.getAppOnlyClient()
+      .then(_this.callRest.bind(_this, method, endpoint, params));
+  }
+
+  this.logger(method, endpoint);
+
   return Q.ninvoke(this.client, method, endpoint, params)
     .then(function(response) {
       return {
@@ -46,17 +63,13 @@ TwitterService.prototype.callRest = function(method, endpoint, params, cb) {
     });
 };
 
-TwitterService.prototype.logger = function logger(method, endpoint, params) {
-  this.log[method] = this.log[method] || {};
-  this.log[method][endpoint] = this.log[method][endpoint] || [];
-
-  this.log[method][endpoint].push({
-    timestamp: new Date(),
-    params: params
-  });
-  return this.logOutput(this.log[method][endpoint]);
-};
-
+/**
+ * Gets a stream from twitter
+ * @param {String} endpoing
+ * @param {Object} params
+ * @param {Function} callback - handles stream onData
+ * @param {Function} errorCallback - handles stream onError
+ **/ 
 TwitterService.prototype.getStream = function(endpoint, params, callback, errorCallback) {
   params = params || {};
 
@@ -67,13 +80,9 @@ TwitterService.prototype.getStream = function(endpoint, params, callback, errorC
     });
 };
 
-function createToken(key, secret) {
-  var token = encodeURIComponent(key) + ':' +
-    encodeURIComponent(secret);
-  return new Buffer(token).toString('base64');
-}
+TwitterService.prototype.getAppOnlyClient = function getAppOnlyClient(config) {
+  var _this = this;
 
-function getAppOnlyClient(config) {
   if (!(config.consumer_key && config.consumer_secret)) {
     throw new Error('Config required');
   }
@@ -95,15 +104,20 @@ function getAppOnlyClient(config) {
     .then(function(response) {
       var body = JSON.parse(response[0].body);
       assign(config, { bearer_token: body.access_token });
-      var client = new Twitter(config);
-      return new TwitterService({client: client});
+      _this.client = new Twitter(config);
+      return _this.client;
     })
     .catch(function(err) {
       console.error('Error in requesting twitter bearer token:', err, err.stack);
     });
-}
+};
 
-function invalidateToken(config) {
+/**
+ * Invalidates a bearer or access token for twitter api calls
+ * @param {Object} config
+ *
+ **/ 
+TwitterService.prototype.invalidateToken = function invalidateToken(config) {
   if (!(config.consumer_key && config.consumer_secret)) {
     throw new Error('Config required');
   }
@@ -130,13 +144,24 @@ function invalidateToken(config) {
     .catch(function(err) {
       console.error('Error in requesting twitter bearer token:', err, err.stack);
     });
+};
 
-}
-
+// This was misnamed, so it's deprecated.  Should be createInstance
 TwitterService.prototype.createTwitterClient = function createTwitterClient(config) {
+  return this.createInstance(config);
+};
+
+/**
+ * Creates a new instance of the TwitterService client
+ * @param {Object} config
+ **/ 
+TwitterService.prototype.createInstance = function createInstance(config) {
   if (!config.access_token_key && !config.access_token_secret) {
     try {
-      return getAppOnlyClient(config);
+      return this.getAppOnlyClient(config)
+        .then(function(client) {
+          return new TwitterService({client: client}); 
+        });
     } catch (ex) {
       console.error('Exception creating twitter client:', ex);
       throw new Error(ex);
@@ -146,6 +171,28 @@ TwitterService.prototype.createTwitterClient = function createTwitterClient(conf
     return Q(twitterClient);
   }
 };
+
+/**
+ * Outputs log information for twitter calls
+ * @param {String} method
+ * @param {String} endpoint
+ **/
+TwitterService.prototype.logger = function logger(method, endpoint) {
+  this.log[method] = this.log[method] || {};
+  this.log[method][endpoint] = this.log[method][endpoint] || [];
+
+  this.log[method][endpoint].push({
+    timestamp: new Date()
+  });
+  return this.logOutput(this.log);
+};
+
+// Creates a token for oauth token
+function createToken(key, secret) {
+  var token = encodeURIComponent(key) + ':' +
+    encodeURIComponent(secret);
+  return new Buffer(token).toString('base64');
+}
 
 module.exports = TwitterService;
 
